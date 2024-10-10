af780f6828ff7f295d82c8c639253d06738359a2
Dhanush Ravichandran <927621bcs026@mkce.ac.in>
2024-10-10 09:15:25 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Nav bar </h2>
+      <h1>hai da ajay</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Counters.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import Counter from './Counter';
+
+class Components extends Component {
+    state = { 
+        counters:[
+            {id:1, value: 1},
+            {id:2, value: 2},
+            {id:3, value: 3}
+        ]
+     } 
+    render() { 
+        return (
+            <div>
+            <h1>Nav Bar: {}</h1>
+            {this.state.counters.map(counter => <Counter key={counter.id} value={counter.value} selectes={true}>
+                <h4>Title #{this.state.counters.id} </h4></Counter> )}
+            </div>
+        );
+    }
+}
+ 
+export default Components;
