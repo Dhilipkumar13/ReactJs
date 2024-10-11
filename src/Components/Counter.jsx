119e0788331c09d73b11d6f39a14c37ded48eac0
Dhilipkumar13 <927621bcs026@mkce.ac.in>
2024-10-11 10:33:31 +0530

    Destructing Arugument

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -3,12 +3,15 @@ import React, { Component } from 'react';
 class Counter extends Component {
     
     render() { 
+
+        const { counter , onInc , onDec , onDelete } = this.props ;
+
         return (
             <div>
-                <h2 className={this.getBadgeClasses()}>Count: {this.props.counter.value}</h2>
-                <button className='btn btn-primary m-2' onClick={() => this.props.onInc(this.props.counter.id)}>Increment</button>
-                <button className='btn btn-secondary m-2' onClick={() => this.props.onDec(this.props.counter.id)}>Decrement</button>
-                <button className='btn btn-danger m-2' onClick={() => this.props.onDelete(this.props.counter.id)}>Delete</button>
+                <h2 className={this.getBadgeClasses()}>Count: {counter.value}</h2>
+                <button className='btn btn-primary m-2' onClick={() => onInc(counter.id)}>Increment</button>
+                <button className='btn btn-secondary m-2' onClick={() => onDec(counter.id)}>Decrement</button>
+                <button className='btn btn-danger m-2' onClick={() => onDelete(counter.id)}>Delete</button>
             </div>
         );
     } 
--- src/Components/Counters.jsx
+++ src/Components/Counters.jsx
@@ -2,20 +2,23 @@ import React, { Component } from 'react';
 import Counter from './Counter';
 
 class Counters extends Component {
-    
+
     render() { 
+
+        const { onReset , onDelete , onInc , onDec } = this.props;
+
         return (
             <div>
-                <button className='btn btn-warning m-3' onClick={this.props.onReset}> Reset </button>
+                <button className='btn btn-warning m-3' onClick={onReset}> Reset </button>
                 
                 {
                     this.props.counters.map(counter => 
                         <Counter 
                             key={counter.id} 
                             counter={counter}  
-                            onDelete={this.props.onDelete} 
-                            onInc={this.props.onInc}
-                            onDec={this.props.onDec}
+                            onDelete={onDelete} 
+                            onInc={onInc}
+                            onDec={onDec}
                         >
                             <h4>Title #{counter.id} </h4>
                         </Counter> 
